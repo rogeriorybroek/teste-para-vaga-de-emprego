abe9ba3d7c2b28d8c1720d4fc37bf3e9882bca59
Rogerio Van Rybroek Junior <rogeriorybroek@gmail.com>
2022-08-20 17:57:59 -0300

    adicionando validacao 2 form

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const formulario = document.querySelector('#form')
 const nome = document.querySelector('#name')
 const btnMore = document.querySelector('.btn-more')
 const produto = document.querySelector('.wrapper')
+const form2 = document.querySelector('#form-compartilhar')
 
 
 formulario.onsubmit = validacaoForm
@@ -12,58 +13,66 @@ function validacaoForm(event) {
     let temErro = false
 
     const inputName = document.forms['form']['name']
-
     if (!inputName.value) {
         temErro = true
         const span = inputName.nextSibling.nextSibling
         span.classList.remove('hidden')
         span.innerText = 'Preencha o campo nome'
+        inputName.style.border = '1px solid red'
     } else {
         const span = inputName.nextSibling.nextSibling
         span.classList.add('hidden')
         span.innerText = ''
+        inputName.style.border = '1px solid #707070'
     }
 
     const inputEmail = document.forms['form']['email']
-
     if (!inputEmail.value) {
         temErro = true
         const span = inputEmail.nextSibling.nextSibling
         span.classList.remove('hidden')
         span.innerText = 'Preencha o campo e-mail'
+        inputEmail.style.border = '1px solid red'
     } else {
         const span = inputEmail.nextSibling.nextSibling
         span.classList.add('hidden')
         span.innerText = ''
+        inputEmail.style.border = '1px solid #707070'
     }
 
     const inputCpf = document.forms['form']['cpf']
-
-    
     if (!inputCpf.value || inputCpf.value.length != 11) {
         temErro = true
         const span = inputCpf.nextSibling.nextSibling
         span.classList.remove('hidden')
         span.innerText = 'Preencha o campo cpf corretamente'
+        inputCpf.style.border = '1px solid red'
     } else {
         const span = inputCpf.nextSibling.nextSibling
         span.classList.add('hidden')
         span.innerText = ''
+        inputCpf.style.border = '1px solid #707070'
     }
 
     if (temErro) {
         return
     }    
+    
+    const formTarget = event.target
 
-    formularioValidado()
+    formularioValidado(formTarget)
 }
 
-function formularioValidado() {
-    formulario.style.display = 'none'
+function formularioValidado(formTarget) {
 
-    const validado = formulario.nextSibling.nextSibling
+    formTarget.style.display = 'none'
 
-    validado.innerHTML = `<h2>Obrigado ${nome.value}, cadastro feito com sucesso!</h2>`
+    const validado = formTarget.nextSibling.nextSibling
+    if (formTarget.id === 'form') {
+        validado.innerHTML = `<h2>Obrigado ${nome.value}, cadastro feito com sucesso!</h2>`
+    } else {
+        validado.innerHTML = `<h2>Obrigado, o email foi enviado com sucesso!</h2>`
+    }
 }
 
 // fetch
@@ -73,6 +82,7 @@ fetch(url)
     .then(transformarEmJson)
     .then(exibirNaTela)
     .then(alimentarHtml)
+    .then(changePage)
 
 function transformarEmJson(response) {
     return response.json()
@@ -90,7 +100,6 @@ function alimentarHtml(dados) {
     for (let product of products) {
         htmlProdutos += `
             <div class="product">
-
                 <div class="img">
                     <img src="${product.image}">
                 </div>
@@ -110,14 +119,62 @@ function alimentarHtml(dados) {
                 ou ${product.installments.count}x de R$${product.installments.value}
                 </p>
                 <button class="btn-comprar">Comprar</button>
-
             </div>
         `
     }
 
     produto.innerHTML = htmlProdutos
-
+    return dados
 }
 
+btnMore.onclick = changePage
 
+function changePage(dados) {
 
+    url = `https://${dados.nextPage}`
+    
+}
+
+form2.onsubmit = validacaoForm2
+
+function validacaoForm2(event) {
+    event.preventDefault()
+
+    let temErro = false
+    
+    const inputFriend = document.forms['form-compartilhar']['friend']
+    if (!inputFriend.value) {
+        temErro = true
+        const span = inputFriend.nextSibling.nextSibling
+        span.classList.remove('hidden')
+        span.innerText = 'preencha o nome do amigo antes de confirmar'
+        inputFriend.style.border = '1px solid red'
+    } else {
+        const span = inputFriend.nextSibling.nextSibling
+        span.classList.add('hidden')
+        span.innerText = ''
+        inputFriend.style.border = '1px solid #707070'
+    }
+
+    const inputEmail = document.forms['form-compartilhar']['friend-email']
+    if (!inputEmail.value) {
+        temErro = true
+        const span = inputEmail.nextSibling.nextSibling
+        span.classList.remove('hidden')
+        span.innerText = 'preencha o nome do amigo antes de confirmar'
+        inputEmail.style.border = '1px solid red'
+    } else {
+        const span = inputEmail.nextSibling.nextSibling
+        span.classList.add('hidden')
+        span.innerText = ''
+        inputEmail.style.border = '1px solid #707070'
+    }
+
+    if(temErro) {
+        return
+    }
+
+    const formTarget = event.target
+
+    formularioValidado(formTarget)
+} 
